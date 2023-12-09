66396bf547e140768bb000653cbf123a3b0db3b6
Ian Hemm <hemmerian@gmail.com>
2023-12-08 19:31:32 -0800

    VERY basic layout of the browser layout

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use std::env;
-use std::error::Error;
-
 use gtk4 as gtk;
-use gtk::{prelude::*, ScrolledWindow};
-use gtk::{glib, Application, ApplicationWindow};
+use gtk::prelude::*;
+use gtk::glib;
 use webkit6 as webkit;
 use webkit::prelude::*;
 
@@ -17,14 +14,46 @@ fn main() -> glib::ExitCode {
         .build();
 
     app.connect_activate(move |app| {
+        // all declarative currently. ill move to xml format later(tm)
+
+        // containing box
         let vbox = gtk::Box::builder()
             .orientation(gtk::Orientation::Vertical)
             .homogeneous(false)
             .build();
 
+
+
+
+        // the URL and search bar
+        let toolbox = gtk::Box::builder()
+            .orientation(gtk::Orientation::Horizontal)
+            .homogeneous(false)
+            .build();
+        let back_button = gtk::Button::builder()
+            .icon_name("back")
+            .build();
+        let forward_button = gtk::Button::builder()
+            .icon_name("forward")
+            .build();
+        let home_button = gtk::Button::builder()
+            .icon_name("home")
+            .build();
+
+        let url_text_input = gtk::Entry::builder().build();
+        let search_text_input = gtk::Entry::builder().build();
+
+        toolbox.append(&back_button);
+        toolbox.append(&forward_button);
+        toolbox.append(&home_button);
+
+        toolbox.append(&url_text_input);
+
         let headerbar = gtk::HeaderBar::builder()
             .show_title_buttons(false)
             .build();
+        headerbar.pack_start(&toolbox);
+        headerbar.pack_end(&search_text_input);
 
         let webview = webkit::WebView::new();
         webview.load_uri(&url_request);
